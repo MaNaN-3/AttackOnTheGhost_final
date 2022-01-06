cdc863dc080ece8de21a30513fa7a940c40b1baf
aakash2khot <79003717+aakash2khot@users.noreply.github.com>
2022-01-06 18:52:58 +0530

    Update SDL2SoundEffects.cpp

--- AttackOnTheGhost/src/SDL2SoundEffects.cpp
+++ AttackOnTheGhost/src/SDL2SoundEffects.cpp
@@ -7,14 +7,14 @@ SDL2SoundEffects::SDL2SoundEffects()
     // init sound
     SDL_Init(SDL_INIT_AUDIO);
 
-    int audio_rate = 22050;
-    Uint16 audio_format = AUDIO_S16SYS;
-    int audio_channels = 2;
-    int audio_buffers = 4096;
+    int rate = 22050;
+    Uint16 format = AUDIO_S16SYS;
+    int channels = 2;
+    int buffers = 4096;
 
-    if (Mix_OpenAudio(audio_rate, audio_format, audio_channels, audio_buffers) != 0)
+    if (Mix_OpenAudio(rate, format, channels, buffers) != 0)
     {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't init audio: %s", Mix_GetError());
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "%s", Mix_GetError());
         exit(-1);
     }
 }
@@ -24,30 +24,26 @@ SDL2SoundEffects::~SDL2SoundEffects()
     SDL_Quit();
 }
 
-void SDL2SoundEffects::addSoundEffect(const char *path)
+void SDL2SoundEffects::addSoundEffect(const char *add)
 {
-    Mix_Chunk *tmpChunk = Mix_LoadWAV(path);
+    Mix_Chunk *tmpChunk = Mix_LoadWAV(add);
 
     if (tmpChunk != nullptr)
     {
         mSoundEffectBank.push_back(tmpChunk);
-        std::cout << (mSoundEffectBank.size() - 1) << " Sound is Ready, path: " << path << '\n';
     }
     else
     {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't init audio: %s", Mix_GetError());
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "%s", Mix_GetError());
     }
 }
 
-void SDL2SoundEffects::playSoundEffect(const int which) const
+void SDL2SoundEffects::playSoundEffect(const int play) const
 {
-    if (which > mSoundEffectBank.size() - 1)
+    if (play > mSoundEffectBank.size() - 1)
     {
-        std::cout << "Sound out of range.\n";
         return;
     }
-    if (which >= 0)
-        Mix_PlayChannel(-1, mSoundEffectBank[which], 0);
-
-    std::cout << "Played Sound: " << which << '\n';
+    if (play >= 0)
+        Mix_PlayChannel(-1, mSoundEffectBank[play], 0);
 }
